9a7371bbfd56be57bb07807fb3c1775e861aa98c
ahmedtalaatcv-hub <ahmedtalaat.cv@gmail.com>
2026-07-11 06:23:28 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+const { exec } = require("child_process");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/youtube", (req, res) => {
+
+    const url = req.query.url;
+
+    if (!url) {
+        return res.status(400).json({
+            error: "Missing url"
+        });
+    }
+
+    const cmd = `yt-dlp -g "${url}"`;
+
+    exec(cmd, (error, stdout, stderr) => {
+
+        if (error) {
+
+            return res.status(500).json({
+                error: stderr || error.message
+            });
+
+        }
+
+        const lines = stdout
+            .trim()
+            .split(/\r?\n/)
+            .filter(x => x);
+
+        if (!lines.length) {
+
+            return res.status(500).json({
+                error: "No stream found"
+            });
+
+        }
+
+        const titleCmd = `yt-dlp --get-title "${url}"`;
+
+exec(titleCmd, (e2, titleOut) => {
+
+    res.json({
+
+        youtube: url,
+        title: titleOut.trim(),
+        stream: lines[0],
+        updated: Date.now()
+
+    });
+
+});
+
+    });
+
+});
+
+app.listen(3000, () => {
+
+    console.log("================================");
+    console.log("YouTube API Running");
+    console.log("http://localhost:3000");
+    console.log("================================");
+
+});
